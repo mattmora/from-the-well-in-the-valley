589a5872b8e108da47ce30defcefed2df8f1a072
Matt <mw4854@nyu.edu>
2022-11-28 00:14:53 -0500

    old action system scripts

---
+++ Assets/__Scripts/Actions.cs
@@ -0,0 +1,486 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PlayerController;
+
+// For grouping actions that should be executed in mutually exclusive cases
+// Cases should be defined as enums, such as PlayerController.PlayerState
+public class PlayerActionSet : Dictionary<Enum, PlayerAction>
+{
+    public PlayerActionSet(params PlayerAction[] actions)
+    {
+        foreach (PlayerAction action in actions)
+        {
+            Add(action.correspondingPlayerState, action);
+        }
+    }
+
+    public void StartActionFor(Enum id)
+    {
+        this[id].StartAction();
+    }
+}
+
+// An action is composed of several segments which run in sequence as coroutines
+// This could be implemented with a single coroutine per action but separating into 
+// segments allows us to better take advantage of inheritance
+[RequireComponent(typeof(PlayerController))]
+public abstract class PlayerAction
+{
+    public enum ActionFlow
+    {
+        Free = 0, // The starting action and playing action play freely
+        Blocks, // The playing action blocks the starting action
+        CanceledBy, // The starting action cancels the playing action
+        // PausedBy? PrerequisiteTo? WaitFor?
+    }
+
+    protected Dictionary<Type, ActionFlow> currentFlow;
+
+    [Serializable]
+    public class Segment
+    {
+        public string name;
+        public Action<Rigidbody> process;
+        public int numFrames;
+        public Dictionary<Type, ActionFlow> typeFlows;
+
+        public Segment(string n, Action<Rigidbody> p, int f, ActionFlow flow)
+        {
+            name = n;
+            process = p;
+            numFrames = f;
+            typeFlows = new Dictionary<Type, ActionFlow>();
+            foreach (Type type in allActionTypes)
+            {
+                typeFlows.Add(type, flow);
+            }
+        }
+    }
+
+    public Segment[] segments; // All segments, including start up and end lag if they exist
+    public int segmentIndex;
+    public int segmentFrame;
+    private bool stopped;
+
+    protected string actionName;
+
+    protected PlayerController owner;
+
+    public PlayerState correspondingPlayerState;
+
+    protected Func<bool> playCheck = () => true;
+
+    protected Action<Rigidbody> process;
+
+    // All types of actions derived from PlayerAction
+    public static readonly HashSet<Type> allActionTypes;
+
+    static PlayerAction()
+    {
+        allActionTypes = new HashSet<Type>();
+
+        foreach (Type type in Assembly.GetAssembly(typeof(PlayerAction)).GetTypes())
+        {
+            if (type.IsClass && !type.IsAbstract && type.IsSubclassOf(typeof(PlayerAction)))
+            {
+                allActionTypes.Add(type);
+            }
+        }
+    }
+
+    public PlayerAction(int n)
+    {
+        // Array of segments of this action, which will run sequentially
+        segments = new Segment[n];
+        process = EmptyProcess;
+    }
+
+    public void Initialize(PlayerController pc, string name)
+    {
+        Debug.Log(name + " Initialize");
+        owner = pc;
+        actionName = name;
+    }
+
+    // Should be called from Update and will set physical processes to be called in FixedUpdate
+    public void StartAction(int startingSegment = 0)
+    {
+        if (!playCheck()) return;
+
+        List<PlayerAction> actionsToCancel = new List<PlayerAction>();
+        foreach (PlayerAction action in owner.currentActions)
+        {
+            ActionFlow actionFlow = action.currentFlow[GetType()];
+            switch (actionFlow)
+            {
+                case ActionFlow.Free:
+                    break;
+                case ActionFlow.Blocks:
+                    Debug.Log(action.actionName + " BLOCKED " + actionName);
+                    return;
+                case ActionFlow.CanceledBy:
+                    actionsToCancel.Add(action);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        foreach (PlayerAction action in actionsToCancel)
+        {
+            Debug.Log(action.actionName + " CANCELED BY " + actionName);
+            action.StopAction();
+        }
+
+        Debug.Log(actionName + " PLAYED");
+
+        stopped = false;
+        owner.currentActions.Add(this);
+        segmentIndex = startingSegment;
+        PreAction();
+        StartCurrentSegment();
+    }
+
+    public void StopAction()
+    {
+        if (stopped) return;
+        Debug.Log(actionName + " STOPPED");
+        segmentIndex = segments.Length-1;
+        EndCurrentSegment();
+        stopped = true;
+    }
+
+    private void EndAction()
+    {
+        process = EmptyProcess;
+        PostAction();
+        owner.currentActions.Remove(this);
+        if (!stopped) Debug.Log(actionName + " FINISHED");
+    }
+
+    // Anything that might need to be done as the full action ends e.g. GroundedDodge resetting the owner drag
+    protected virtual void PreAction() { }
+
+    // Anything that might need to be done as the full action ends e.g. GroundedDodge resetting the owner drag
+    protected virtual void PostAction() { }
+
+    public void Process(Rigidbody rb)
+    {
+        if (segmentIndex < segments.Length)
+        {
+            if (segmentFrame >= segments[segmentIndex].numFrames)
+            {
+                segmentIndex++;
+                if (segmentIndex < segments.Length)
+                {
+                    StartCurrentSegment();
+                }
+                else EndAction();
+            }
+            process(rb);
+        }
+    }
+
+    // Might not be necessary but we'll set the process to this when
+    // the action isn't running in case there's a possibility for stray calls
+    protected void EmptyProcess(Rigidbody rb) { }
+
+    protected void MinimalProcess(Rigidbody rb)
+    {
+        // Probably won't do anything here in the base aside from advancing the frame
+        segmentFrame++;
+    }
+    private void StartCurrentSegment()
+    {
+        Debug.Log(actionName + " " + segments[segmentIndex].name);
+        segmentFrame = 0;
+        process = segments[segmentIndex].process;
+        currentFlow = segments[segmentIndex].typeFlows;
+    }
+
+    protected void EndCurrentSegment()
+    {
+        if (segmentIndex < segments.Length) segmentFrame = segments[segmentIndex].numFrames;
+    }
+
+    protected void SetActionFlowForSegments(int i, ActionFlow flow, params Type[] types)
+    {
+        foreach (Type type in types)
+        {
+            segments[i].typeFlows[type] = flow;
+        }
+    }
+
+    protected void SetActionFlowForSegments(int[] segIndices, ActionFlow flow, params Type[] types)
+    {
+        foreach (int i in segIndices)
+        {
+            SetActionFlowForSegments(i, flow, types);
+        }
+    }
+}
+
+//===============================================================================================================
+// DERIVED ACTIONS ==============================================================================================
+//===============================================================================================================
+
+[Serializable]
+public class MoveAction : PlayerAction
+{
+    public float groundedAcceleration = 100;
+    public float aerialAcceleration = 100;
+
+    public MoveAction() : base(1)
+    {
+        segments[0] = new Segment("Move", MoveProcess, 1, ActionFlow.Free);
+
+        // Blocks self
+        SetActionFlowForSegments(0, ActionFlow.Blocks, typeof(MoveAction));
+        // Cancelled by run
+        SetActionFlowForSegments(0, ActionFlow.CanceledBy, typeof(RunAction), typeof(GroundedDodgeAction), typeof(AerialDodgeAction), typeof(LandAction)); 
+    }
+
+    private void MoveProcess(Rigidbody rb)
+    {
+        // Apply normal acceleration based on direction input
+        float acceleration = owner.currentPlayerState == PlayerState.Grounded ? groundedAcceleration : aerialAcceleration;
+        Vector3 movement = acceleration * Vector3.Normalize(owner.forward + owner.right);
+        rb.AddForce(movement);
+        //segmentFrame++;
+    }
+}
+
+[Serializable]
+public class RunAction : PlayerAction
+{
+    private readonly float initialAcceleration = 100;
+    private readonly float accelerationInc = 10;
+    private float acceleration;
+
+    public RunAction() : base(3)
+    {
+        segments[0] = new Segment("InitialDash", InitialDashProcess, 4, ActionFlow.CanceledBy);
+        segments[1] = new Segment("Run", MoveProcess, 1, ActionFlow.CanceledBy);
+        segments[2] = new Segment("Stop", MinimalProcess, 4, ActionFlow.Blocks);
+
+        SetActionFlowForSegments(0, ActionFlow.Blocks, typeof(MoveAction));
+        SetActionFlowForSegments(1, ActionFlow.Blocks, typeof(RunAction), typeof(MoveAction));
+        SetActionFlowForSegments(new int[] { 0, 1, 2 }, ActionFlow.Blocks, typeof(RunAction));
+    }
+
+    protected override void PreAction()
+    {
+        acceleration = initialAcceleration;
+    }
+
+    private void InitialDashProcess(Rigidbody rb)
+    {
+        Vector3 movement = acceleration * Vector3.Normalize(owner.forward + owner.right);
+        rb.AddForce(movement);
+        acceleration += accelerationInc;
+        segmentFrame++;
+    }
+
+    private void MoveProcess(Rigidbody rb)
+    {
+        Vector3 movement = acceleration * Vector3.Normalize(owner.forward + owner.right);
+        rb.AddForce(movement);
+        //segmentFrame++;
+    }
+}
+
+[Serializable]
+public class GroundedJumpAction : PlayerAction
+{
+    public float shortJumpForce = 5;
+    public float fullJumpForce = 10;
+    private float jumpForce;
+    public GroundedJumpAction() : base(3)
+    {
+        correspondingPlayerState = PlayerState.Grounded;
+        segments[0] = new Segment("JumpSquat", JumpSquatProcess, 3, ActionFlow.Blocks);
+        segments[1] = new Segment("Jump", JumpProcess, 1, ActionFlow.Blocks);
+        segments[2] = new Segment("Rising", RisingProcess, 1, ActionFlow.Free);
+
+        SetActionFlowForSegments(2, ActionFlow.CanceledBy, typeof(AerialDodgeAction), typeof(AerialJumpAction));
+        SetActionFlowForSegments(2, ActionFlow.Blocks, typeof(LandAction), typeof(FallOffAction));
+    }
+
+    private void JumpSquatProcess(Rigidbody rb)
+    {
+        if (Input.GetButton("Jump"))
+        {
+            jumpForce = fullJumpForce;
+        }
+        else
+        {
+            jumpForce = shortJumpForce;
+        }
+        segmentFrame++;
+    }
+
+    // Called in the owner's FixedUpdate for the duration of the segment
+    private void JumpProcess(Rigidbody rb)
+    {
+        Vector3 xzVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.velocity = xzVelocity;
+        Vector3 jump = jumpForce * Vector3.up;
+        rb.AddForce(jump, ForceMode.VelocityChange);
+        segmentFrame++;
+    }
+
+    private void RisingProcess(Rigidbody rb)
+    {
+        if (rb.velocity.y <= 0)
+        {
+            EndCurrentSegment();
+        }
+    }
+}
+
+[Serializable]
+public class AerialJumpAction : PlayerAction
+{
+    public float jumpForce = 10;
+
+    public AerialJumpAction() : base(1)
+    {
+        correspondingPlayerState = PlayerState.Aerial;
+        segments[0] = new Segment("Jump", JumpProcess, 1, ActionFlow.Blocks);
+        playCheck = () => (owner.aerialJumpCount > 0);
+    }
+
+    // Called in the owner's FixedUpdate for the duration of the segment
+    private void JumpProcess(Rigidbody rb)
+    {
+        owner.DecrementJumpCount();
+        Vector3 xzVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.velocity = xzVelocity;
+        Vector3 jump = jumpForce * Vector3.up;
+        rb.AddForce(jump, ForceMode.VelocityChange);
+        segmentFrame++;
+    }
+}
+
+[Serializable]
+public class GroundedDodgeAction : PlayerAction
+{
+    public float dodgeForce = 10;
+    public float dodgeDrag = 0;
+    private float ownerDrag;
+
+    public GroundedDodgeAction() : base(2)
+    {
+        correspondingPlayerState = PlayerState.Grounded;
+        segments[0] = new Segment("Dodge", DodgeProcess, 20, ActionFlow.Blocks);
+        segments[1] = new Segment("EndLag", StopProcess, 8, ActionFlow.Blocks);
+        SetActionFlowForSegments(0, ActionFlow.CanceledBy, typeof(FallOffAction));
+    }
+
+    private void DodgeProcess(Rigidbody rb)
+    {
+        if (segmentFrame == 0)
+        {
+            rb.AddForce(-rb.velocity, ForceMode.VelocityChange);
+            //rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            ownerDrag = owner.groundedDrag;
+            owner.groundedDrag = dodgeDrag;
+
+            Vector3 dodge = dodgeForce * Vector3.Normalize(owner.forwardAlt.normalized + owner.rightAlt.normalized);
+            rb.AddForce(dodge, ForceMode.VelocityChange);
+        }
+        segmentFrame++;
+    }
+
+    private void StopProcess(Rigidbody rb)
+    {
+        //rb.velocity = Vector3.zero;
+        if (segmentFrame == 0)  rb.AddForce(-rb.velocity, ForceMode.VelocityChange);
+        segmentFrame++;
+    }
+
+    protected override void PostAction()
+    {
+        Debug.Log(actionName + " PostAction");
+        owner.groundedDrag = ownerDrag;
+    }
+}
+
+
+[Serializable]
+public class AerialDodgeAction : PlayerAction
+{
+    public float dodgeForce = 20;
+    public float dodgeDrag = 0;
+    private float ownerDrag;
+    public Vector3 gravity = Vector3.zero;
+    private Vector3 ownerGravity;
+
+    public AerialDodgeAction() : base(2)
+    {
+        correspondingPlayerState = PlayerState.Aerial;
+        segments[0] = new Segment("Dodge", DodgeProcess, 10, ActionFlow.Blocks);
+        segments[1] = new Segment("EndLag", StopProcess, 8, ActionFlow.Blocks);
+        SetActionFlowForSegments(0, ActionFlow.CanceledBy, typeof(LandAction));
+    }
+
+    // Called in the owner's FixedUpdate for the duration of the segment
+    private void DodgeProcess(Rigidbody rb)
+    {
+        if (segmentFrame == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            ownerDrag = owner.aerialDrag;
+            owner.aerialDrag = dodgeDrag;
+            ownerGravity = owner.gravity;
+            owner.gravity = gravity;
+
+            Vector3 dodge = dodgeForce * Vector3.Normalize(owner.forwardAlt.normalized + owner.rightAlt.normalized);
+            dodge = new Vector3(dodge.x, -dodge.magnitude, dodge.z);
+            rb.AddForce(dodge, ForceMode.VelocityChange);
+        }
+        segmentFrame++;
+    }
+
+    private void StopProcess(Rigidbody rb)
+    {
+        //rb.velocity = Vector3.zero;
+        if (segmentFrame == 0) rb.AddForce(-rb.velocity, ForceMode.VelocityChange);
+        segmentFrame++;
+    }
+
+    protected override void PostAction()
+    {
+        Debug.Log(actionName + " PostAction");
+        owner.aerialDrag = ownerDrag;
+        owner.gravity = ownerGravity;
+    }
+}
+
+[Serializable]
+public class LandAction : PlayerAction
+{
+    public LandAction() : base(1)
+    {
+        segments[0] = new Segment("Land", LandProcess, 3, ActionFlow.Blocks);
+    }
+
+    private void LandProcess(Rigidbody rb)
+    {
+        segmentFrame++;
+    }
+}
+
+// Not exactly an action, but allows for canceling other actions when going from grounded to aerial
+[Serializable]
+public class FallOffAction : PlayerAction
+{
+    public FallOffAction() : base(1)
+    {
+        // TODO: Implement variable segment length based on player state (e.g. in melee, 10 frames landing lag from helpless state after air dodge, fewer frames from normal state)
+        segments[0] = new Segment("Falloff", MinimalProcess, 1, ActionFlow.Free);
+    }
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,287 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    public enum PlayerState
+    {
+        None,
+        Grounded,
+        Aerial
+    }
+
+    public PlayerState currentPlayerState;
+
+    public Vector3 gravity = new Vector3(0, -40, 0);
+    public Vector3 activeGravity;
+
+    // Instead of friction, to avoid increased slow down when air dodging into ground
+    public float traction = 10.0f;
+
+    public float groundedDrag = 1;
+    public float aerialDrag = 1;
+
+    public float aerialControl = 1;
+    public float postDodgeControl = 0.1f;
+
+    public Vector3 groundedPosition;
+    private float lastGroundedY;
+
+    [Serializable]
+    public class PlayerInput
+    {
+        public float vertical;
+        public float horizontal;
+        public float verticalAlt;
+        public float horizontalAlt;
+    }
+
+    [Serializable]
+    public class PlayerEvent
+    {
+        public bool jump;
+        public bool dodge;
+        public bool land;
+        public bool falloff;
+    }
+
+    public PlayerInput inputs;
+    public PlayerEvent events;
+
+    public Vector3 forward;
+    public Vector3 right;
+    public Vector3 forwardAlt;
+    public Vector3 rightAlt;
+
+    public int numAerialJumps = 1;
+    public int aerialJumpCount;
+
+    private Vector3 isoForward;
+    private Vector3 isoRight;
+
+    private Rigidbody rb;
+    private BoxCollider coll;
+    private Material mat;
+
+    public Color aerialEmissiveColor = new Color(0.4f, 0.4f, 0.4f, 0.4f);
+
+    public MoveAction move;
+    public RunAction run;
+    public GroundedJumpAction groundedJump;
+    public AerialJumpAction aerialJump;
+    public GroundedDodgeAction groundedDodge;
+    public AerialDodgeAction aerialDodge;
+    public LandAction land;
+    public FallOffAction falloff;
+
+    private PlayerActionSet jump;
+    private PlayerActionSet dodge;
+
+    public HashSet<PlayerAction> currentActions;
+
+    void Reset()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isoForward = Vector3.Normalize(Vector3.forward + Vector3.right);
+        isoRight = Vector3.Normalize(Vector3.right + Vector3.back);
+
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<BoxCollider>();
+        mat = GetComponent<Renderer>().material;
+
+        currentActions = new HashSet<PlayerAction>();
+
+        FieldInfo[] fields = GetType().GetFields();
+        foreach (FieldInfo field in fields)
+        {
+            if (field.FieldType.IsSubclassOf(typeof(PlayerAction)))
+            {
+                PlayerAction action = (PlayerAction)field.GetValue(this);
+                action.Initialize(this, field.Name);
+            }
+        }
+
+        jump = new PlayerActionSet(groundedJump, aerialJump);
+        dodge = new PlayerActionSet(groundedDodge, aerialDodge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        if (currentPlayerState == PlayerState.Grounded)
+        {
+            mat.SetColor("_EmissionColor", Color.black);
+            aerialJumpCount = numAerialJumps;
+        }
+        else if (currentPlayerState == PlayerState.Aerial)
+        {
+            mat.SetColor("_EmissionColor", aerialEmissiveColor);
+        }
+
+        mat.color = Color.blue;
+
+        if (currentActions.Overlaps(jump.Values))
+        {
+            mat.color = Color.red;
+        }
+        if (currentActions.Overlaps(dodge.Values))
+        {
+            mat.color = Color.green;
+        }
+        if (currentActions.Contains(land))
+        {
+            mat.color = Color.gray;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Check for grounding
+        GroundCheck();
+
+        // Determine last grounded y position for the camera to follow
+        if (currentPlayerState == PlayerState.Grounded) lastGroundedY = transform.position.y;
+        groundedPosition = new Vector3(transform.position.x, lastGroundedY, transform.position.z);
+
+        // Get isometrically adjusted input vectors
+        forward = inputs.vertical * isoForward;
+        right = inputs.horizontal * isoRight;
+
+        forwardAlt = inputs.verticalAlt * isoForward;
+        rightAlt = inputs.horizontalAlt * isoRight;
+
+        HashSet<PlayerAction> frameActions = new HashSet<PlayerAction>(currentActions);
+        foreach (PlayerAction action in frameActions)
+        {
+            action.Process(rb);
+        }
+
+        // Apply environment forces
+        //==============================
+        // Apply drag, only horizontally
+        Vector3 xzVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        float drag = currentPlayerState == PlayerState.Grounded ? groundedDrag : aerialDrag;
+        float dragMag = xzVelocity.sqrMagnitude * drag;
+        Vector3 env = dragMag * -xzVelocity.normalized;
+
+        rb.AddForce(Mathf.Min(traction * Time.fixedDeltaTime, xzVelocity.magnitude) * -xzVelocity.normalized, ForceMode.VelocityChange);
+
+        // Apply gravity
+        env += activeGravity;
+
+        // Add the composite environmental forces
+        rb.AddForce(env);
+
+        // Max move speed = sqrt((acceleration - traction) / drag)
+        // Dodge distance = -traction*0.5*(dodgeFrames/fr)^2 + dodgeV0*(dodgeFrames/fr)
+    }
+
+    void GetInput()
+    {
+        float control = currentPlayerState == PlayerState.Grounded ? 1.0f : aerialControl;
+
+        inputs.vertical = (Input.GetAxis("Vertical") * control) + (inputs.vertical * (1.0f - control));
+        inputs.horizontal = (Input.GetAxis("Horizontal") * control) + (inputs.horizontal * (1.0f - control));
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            if (Input.GetButton("Dash") && currentPlayerState == PlayerState.Grounded)
+            {
+                run.StartAction();
+            }
+            else
+            {
+                run.StopAction();
+                move.StartAction();
+            }
+        }
+        else
+        {
+            run.StopAction();
+            move.StopAction();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            events.jump = true;
+        }
+
+        inputs.verticalAlt = (Input.GetAxis("VerticalAlt") * control) + (inputs.verticalAlt * (1.0f - control));
+        inputs.horizontalAlt = (Input.GetAxis("HorizontalAlt") * control) + (inputs.horizontalAlt * (1.0f - control));
+        if (Input.GetButtonDown("Dodge"))
+        {
+            events.dodge = true;
+        }
+
+        // Not to sure whether to put this in Update or FixedUpdate
+        if (events.jump)
+        {
+            jump.StartActionFor(currentPlayerState);
+            events.jump = false;
+        }
+        if (events.dodge)
+        {
+            dodge.StartActionFor(currentPlayerState);
+            events.dodge = false;
+        }
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        bool centerGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, (transform.localScale.y * 0.5f) + Physics.defaultContactOffset);//Physics.defaultContactOffset);
+        //Debug.Log(Physics.defaultContactOffset);
+
+        // This only makes sense as long as it's just a 1x1 cube but want to experiment
+        //int cornersGrounded = 0;
+        //if (Physics.Raycast(transform.position + (Vector3.right + Vector3.forward) * 0.5f, Vector3.down, out hit, 0.5f + 0.01f)) cornersGrounded++;
+        //Debug.Log(hit.normal);
+        //if (Physics.Raycast(transform.position + (Vector3.right + Vector3.back) * 0.5f, Vector3.down, out hit, 0.5f + 0.01f)) cornersGrounded++;
+        //if (Physics.Raycast(transform.position + (Vector3.left + Vector3.back) * 0.5f, Vector3.down, out hit, 0.5f + 0.01f)) cornersGrounded++;
+        //if (Physics.Raycast(transform.position + (Vector3.left + Vector3.forward) * 0.5f, Vector3.down, out hit, 0.5f + 0.01f)) cornersGrounded++;
+
+        if (centerGrounded)
+            //if (Physics.BoxCast(transform.position, new Vector3(0.5f - Physics.defaultContactOffset, 0, 0.5f - Physics.defaultContactOffset), Vector3.down, Quaternion.identity, 0.5f + Physics.defaultContactOffset))
+        {
+            if (currentPlayerState == PlayerState.Aerial) events.land = true;
+            currentPlayerState = PlayerState.Grounded;
+            if (hit.rigidbody is null || Math.Abs(hit.rigidbody.velocity.y) < Physics.defaultContactOffset)
+            {
+                activeGravity = Vector3.zero;
+                //activeGravity = gravity;
+            }
+        }
+        else
+        {
+            if (currentPlayerState == PlayerState.Grounded) events.falloff = true;
+            currentPlayerState = PlayerState.Aerial;
+            activeGravity = gravity;
+        }
+
+        // Not to sure where to put this
+        if (events.land)
+        {
+            land.StartAction();
+            events.land = false;
+        }
+        else if (events.falloff)
+        {
+            falloff.StartAction();
+            events.falloff = false;
+        }
+    }
+
+    public void DecrementJumpCount()
+    {
+        if (currentPlayerState == PlayerState.Aerial) aerialJumpCount--;
+    }
+}
